43e0dcc133876a08df21b4059c3d1186bdfa705b
gamggi <gamggi@gmail.com>
2020-05-09 10:45:00 -0300

    zsh pc 이름 제거 및 auto-suggestion 추가

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -11,4 +11,21 @@ echo 'oh-my-zsh 설치 완료'
 
 sed -i "s/robbyrussell/agnoster/g" ~/.zshrc
 
+echo '자동완성 플러그인'
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sed -i "s/(git/(git zsh-autosuggestions/g" ~/.zshrc
+
+echo 'pc 이름 제거'
+
+cat << EOF >> ~/.zshrc
+prompt_context() { 
+  if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then 
+    prompt_segment black default "%(!.%{%F{yellow}%}.)$USER" 
+  fi 
+}
+EOF
+
+
+
 source ~/.zshrc
